b6d304a5c2ebaaf91b3965d86a079e8af84371b5
JakeGrainger <j.p.grainger2@outlook.com>
2022-11-08 15:17:17 +0100

    Add some utilities.

---
+++ src/utils.jl
@@ -0,0 +1,18 @@
+function boundinggrid(geom::Geometry)
+    box = boundingbox(geom)
+    return CartesianGrid(box.min,box.max,dims=grid_res)
+end
+
+function mask(X::PointSet,geom::Geometry)
+    return PointSet([x for x ∈ X if x ∈ geom])
+end
+
+function thin(X, retention_probability)
+	thinned = eltype(X)[]
+	for ξ ∈ X
+		if rand() ≤ retention_probability(ξ)
+			push!(thinned, ξ)
+		end
+	end
+	return thinned
+end
