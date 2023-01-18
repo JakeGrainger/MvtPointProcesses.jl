afc2c251b0d7a871aed4a344586a8d1d705f659a
JakeGrainger <j.p.grainger2@outlook.com>
2023-01-18 10:23:07 +0100

    Add function to expand a box with fixed padding.

--- src/utils.jl
+++ src/utils.jl
@@ -3,6 +3,10 @@ function boundinggrid(geom::Geometry{D,T},grid_res::NTuple{D,Int}=ntuple(d->1000
     return CartesianGrid(box.min,box.max,dims=grid_res)
 end
 
+function expandbox(box::Box, pad::Number)
+	return Box(Point(box.min.coords .- pad), Point(box.max.coords .+ pad))
+end
+
 function mask(X::PointSet,geom::Geometry)
     return PointSet([x for x ∈ X if x ∈ geom])
 end
