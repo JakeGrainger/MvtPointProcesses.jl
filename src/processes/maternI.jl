0eec86cc589eadcd02324b795e470558c7c6c361
JakeGrainger <j.p.grainger2@outlook.com>
2023-01-19 14:50:52 +0100

    Update name of model Now different from index over points.

--- src/processes/maternI.jl
+++ src/processes/maternI.jl
@@ -4,11 +4,11 @@ struct MaternIProcess{T1<:Real,T2<:Real,G<:Geometry} <: PointProcess
 	geom::G
 end
 
-function rand(p::MaternIProcess)
-    proposals = rand(PoissonProcess(p.κ, expandbox(boundingbox(p.geom), p.r))) # generate on a bounding box inflated by the inhibition radius
+function rand(model::MaternIProcess)
+    proposals = rand(PoissonProcess(model.κ, expandbox(boundingbox(model.geom), model.r))) # generate on a bounding box inflated by the inhibition radius
     X = eltype(proposals.items)[]
     for p in proposals.items #  note this could be made more efficient
-        if sum(norm(p.coords.-q.coords) < p.r for q in proposals.items) == 1 # one of these is distance to self
+        if sum(norm(p.coords.-q.coords) < model.r for q in proposals.items) == 1 # one of these is distance to self
             push!(X,p)
         end
     end
