a80c9e696bb17020a4560520c228db734c72c3fa
JakeGrainger <j.p.grainger2@outlook.com>
2023-03-24 12:39:34 +0100

    Add docstring for Thomas processes.

--- src/processes/thomas.jl
+++ src/processes/thomas.jl
@@ -1,3 +1,15 @@
+"""
+    ThomasProcess{D,P,...}
+
+A Thomas process on a `D` dimensional space with `P` variables.
+Multivariate independent Thomas process with different parameters.
+
+# Parameters
+- `κ`: The intensity of the parent process.
+- `σ`: The standard deviation of the point locations around their parent (isotropic Gaussian distributed).
+- `μ`: The mean number of points per cluster.
+- `geom`: Simulation region.
+"""
 struct ThomasProcess{D,P,T,T1<:Real,T2<:NTuple{P,<:Real},T3<:NTuple{P,<:Real},G<:Geometry{D,T}} <: PointProcess{D,P}
     κ::T1
 	σ::T2
